9b393b9c80b96ba0414805c77c2bff54c1f233d9
Colacho <fcolacilli@gmail.com>
2022-09-08 22:14:28 -0300

    carga de animales andando y modificaciones a la pagina

--- cargarperro.php
+++ cargarperro.php
@@ -1,17 +1,39 @@
 <?php
     $con = mysqli_connect('localhost', 'root', '', 'refucan') or die('Error al conectarse');
-    $sql = "INSERT INTO animal
-    VALUES(null, 
-    '".$_POST["nombre"]."', 
-    '".$_POST["raza"]."', 
-    '".$_POST["porte"]."', 
-    '".$_POST["manto"]."', 
-    '".$_POST["rasgos"]."', 
-    '".$_POST["foto"]."', 
-    '".$_POST["dni"]."',
-    )";
+    
+    
+    $sql = "INSERT INTO animales
+    VALUES(
+        null,
+        '".$_POST["nombre"]."', 
+        '".$_POST["sexo"]."', 
+        '".$_POST["dni"]."',
+        '".$_POST["raza"]."', 
+        '".$_POST["porte"]."', 
+        '".$_POST["manto"]."', 
+        '".$_POST["rasgos"]."',
+        '".$_POST["foto"]."',
+        '".$_POST["protectora_id"]."'
+        )";
+
     $resultado = mysqli_query($con, $sql) or die('Error de consulta');
-    mysqli_close($con);
+        
+        $sql2 = "INSERT INTO historia_clinica
+    VALUES(
+        null, 
+        null, 
+        null, 
+        null, 
+        null,
+        null
+        )";
+
+    $resultado = mysqli_query($con, $sql2) or die('Error de consulta');
+
+    
+        
+    
+
     echo 'El registro se guardo exitosamente';
     header('location:index.html');
 ?>
